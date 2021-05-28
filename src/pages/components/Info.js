cc7aec898483243f46e0d5416b18a121fecb7c8d
mczernyk <mczernyk@gmail.com>
2021-05-28 03:12:28 -0400

    more copy revisions

--- src/pages/components/Info.js
+++ src/pages/components/Info.js
@@ -36,9 +36,9 @@ class Info extends React.Component {
             <h2><span className="black-bold">LinkLater</span> is a <span className="blue-bold">social</span> bookmarking app.</h2>
             <div>
             <h3>As the internet continues to get more saturated and fragmented, discovery and curation will become bigger problems for creators and consumers alike.</h3>
-            <h3><span className="blue-bold">LinkLater</span> helps you keep track of content that matters.</h3>
-            <h3>Your <span className="black-bold">Feed</span> is a composite of links that you've saved for later and content sent to you by friends that you can get to on your own time.</h3>
-            <h3>You can organize links from your <span className="black-bold">Feed</span> into <span className="black-bold">Collections</span> or share them with your friends on <span className="blue-bold">LinkLater</span>.</h3>
+            <h3><span className="black-bold">LinkLater</span> helps you keep track of content that matters.</h3>
+            <h3>Your <span className="blue-bold">Feed</span> is a composite of links that you've saved for later and content sent to you by friends that you can get to on your own time.</h3>
+            <h3>You can organize links from your <span className="blue-bold">Feed</span> into <span className="blue-bold">Collections</span> or share them with your friends in app on <span className="black-bold">LinkLater</span>.</h3>
             </div>
 
             <h2></h2>
