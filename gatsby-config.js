03b074633e07cb4c979d1e8600f0c97685717983
mczernyk <mczernyk@gmail.com>
2021-06-24 20:55:51 -0400

    update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#048BFF`,
         theme_color: `#048BFF`,
         display: `minimal-ui`,
-        icon: `src/images/Icon-Thumb-Black-BlueBg.png`,
+        icon: `src/images/Icon-Thumb-Square.png`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
